076334b4528f84f55447ceaabebd2ee194959cb0
Alden Manisan <Alden Manisan@DESKTOP-L77T0DB>
2021-11-12 18:22:31 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿
+namespace Azur_Lane_Exp_Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbCurrentLevel = new System.Windows.Forms.ComboBox();
+            this.cmbTargetLevel = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.cmbRarity = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblExpNeeded = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmbCurrentLevel
+            // 
+            this.cmbCurrentLevel.FormattingEnabled = true;
+            this.cmbCurrentLevel.Location = new System.Drawing.Point(65, 35);
+            this.cmbCurrentLevel.Name = "cmbCurrentLevel";
+            this.cmbCurrentLevel.Size = new System.Drawing.Size(121, 23);
+            this.cmbCurrentLevel.TabIndex = 0;
+            // 
+            // cmbTargetLevel
+            // 
+            this.cmbTargetLevel.FormattingEnabled = true;
+            this.cmbTargetLevel.Location = new System.Drawing.Point(65, 94);
+            this.cmbTargetLevel.Name = "cmbTargetLevel";
+            this.cmbTargetLevel.Size = new System.Drawing.Size(121, 23);
+            this.cmbTargetLevel.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(93, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Current Level";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(93, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Target Level";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(87, 227);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 4;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // cmbRarity
+            // 
+            this.cmbRarity.FormattingEnabled = true;
+            this.cmbRarity.Location = new System.Drawing.Point(65, 156);
+            this.cmbRarity.Name = "cmbRarity";
+            this.cmbRarity.Size = new System.Drawing.Size(121, 23);
+            this.cmbRarity.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(93, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Ship Rarity";
+            // 
+            // lblExpNeeded
+            // 
+            this.lblExpNeeded.AutoSize = true;
+            this.lblExpNeeded.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblExpNeeded.Location = new System.Drawing.Point(147, 194);
+            this.lblExpNeeded.Name = "lblExpNeeded";
+            this.lblExpNeeded.Size = new System.Drawing.Size(34, 21);
+            this.lblExpNeeded.TabIndex = 7;
+            this.lblExpNeeded.Text = "----";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(12, 190);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(129, 25);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Exp Needed: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(261, 264);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblExpNeeded);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmbRarity);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbTargetLevel);
+            this.Controls.Add(this.cmbCurrentLevel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Exp Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbCurrentLevel;
+        private System.Windows.Forms.ComboBox cmbTargetLevel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.ComboBox cmbRarity;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblExpNeeded;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Azur_Lane_Exp_Calculator
+{
+    public partial class Form1 : Form
+    {
+
+        readonly string[] Rarity = { "Normal Ship", "Rainbow Ship" };
+
+        int current;
+        int target;
+        int rarity;
+
+        int[] table;
+
+        int[] experience_table_enjp = {0, 100, 300, 600, 1000, 1500, 2100, 2800, 3600,
+        4500, 5500, 6600, 7800, 9100, 10500, 12000, 13600, 15300, 17100, 19000, 21000,
+        23100, 25300, 27600, 30000, 32500, 35100, 37800, 40600, 43500, 46500, 49600,
+        52800, 56100, 59500, 63000, 66600, 70300, 74100, 78000, 82000, 86200, 90600,
+        95200, 100000, 105000, 110200, 115600, 121200, 127000, 133000, 139200, 145600,
+        152200, 159000, 166000, 173200, 180600, 188200, 196000, 204000, 212300, 220900,
+        229800, 239000, 248500, 258300, 268400, 278800, 289500, 301600, 314140, 327120,
+        340540, 354400, 368700, 383440, 398620, 414240, 430300, 447550, 465375, 483775,
+        502750, 522300, 542425, 563125, 584400, 606250, 628675, 652675, 677875, 704275,
+        733075, 764275, 800275, 842275, 890275, 962275, 1120675, 1170675, 1223675,
+        1279675, 1338675, 1400675, 1465675, 1536675, 1613675, 1696675, 1785675,
+        1880675, 1985675, 2100675, 2225675, 2360675, 2505675, 2665675, 2840675,
+        3030675, 3235675, 3455675, 3696675, 3958675, 4241675, 4545675};
+
+        int[] experience_table_ur = {0, 120, 360, 720, 1200, 1800, 2520, 3360, 4320,
+        5400, 6600, 7920, 9360, 10920, 12600, 14400, 16320, 18360, 20520, 22800, 25200,
+        27720, 30360, 33120, 36000, 39000, 42120, 45360, 48720, 52200, 55800, 59520,
+        63360, 67320, 71400, 75600, 79920, 84360, 88920, 93600, 98400, 103440, 108720,
+        114240, 120000, 126000, 132240, 138720, 145440, 152400, 159600, 167040, 174720,
+        182640, 190800, 199200, 207840, 216720, 225840, 235200, 244800, 254760, 265080,
+        275760, 286800, 298200, 309960, 322080, 334560, 347400, 360600, 374280, 388440,
+        403080, 418200, 433800, 449880, 466440, 483480, 501000, 519000, 537600, 556800,
+        576600, 597000, 618000, 639600, 661800, 684600, 708000, 734000, 761300, 789900,
+        821100, 854900, 893900, 939400, 991400, 1069400, 1241000, 1301000, 1364600,
+        1431800, 1502600, 1577000, 1655000, 1740200, 1832600, 1932200, 2039000,
+        2153000, 2279000, 2417000, 2567000, 2729000, 2903000, 3095000, 3305000,
+        3533000, 3779000, 4043000, 4332200, 4646600, 4986200, 5351000 };
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int levels = 1; levels <= 125; levels++)
+            {
+                cmbCurrentLevel.Items.Add("Level " + levels.ToString());
+                cmbTargetLevel.Items.Add("Level " + levels.ToString());
+            }
+            cmbRarity.Items.AddRange(Rarity);
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            current = cmbCurrentLevel.SelectedIndex + 1;
+            target = cmbTargetLevel.SelectedIndex + 1;
+            rarity = cmbRarity.SelectedIndex;
+
+            calculate_exp(rarity);
+        }
+
+        public void calculate_exp(int shipType)
+        {
+            if (shipType == 0)
+            {
+                table = experience_table_enjp;
+            }
+            else
+            {
+                table = experience_table_ur;
+            }
+            int exp = get_exp_diff(table, current, target);
+
+            if (exp != null)
+            {
+                lblExpNeeded.Text = exp.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Please Input All The Fields");
+            }
+
+
+        }
+
+        public int get_total_exp(int[] table, int level)
+        {
+            if (level > 0 && level <= 125)
+            {
+                return table[level - 1];
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public int get_exp_diff(int[] table, int current, int target)
+        {
+            current = get_total_exp(table, current);
+            target = get_total_exp(table, target);
+
+            return target - current;
+        }
+    }
+}
