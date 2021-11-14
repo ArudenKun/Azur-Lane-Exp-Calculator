b8268f84f35e42515cebe94bcc67453fe582c9ab
Aruden <84431560+ArudenKun@users.noreply.github.com>
2021-11-14 17:01:35 +0800

    Added Exe File Icon Used Code Maid To Optimize Code

--- Form1.cs
+++ Form1.cs
@@ -5,10 +5,10 @@ namespace Azur_Lane_Exp_Calculator
 {
     public partial class Form1 : Form
     {
-        int current, target, rarity;
-        int[] table;
+        private int current, target, rarity;
+        private int[] table;
 
-        readonly int[] experience_table_enjp = {0, 100, 300, 600, 1000, 1500, 2100, 2800, 3600,
+        private readonly int[] experience_table_enjp = {0, 100, 300, 600, 1000, 1500, 2100, 2800, 3600,
         4500, 5500, 6600, 7800, 9100, 10500, 12000, 13600, 15300, 17100, 19000, 21000,
         23100, 25300, 27600, 30000, 32500, 35100, 37800, 40600, 43500, 46500, 49600,
         52800, 56100, 59500, 63000, 66600, 70300, 74100, 78000, 82000, 86200, 90600,
@@ -22,7 +22,7 @@ namespace Azur_Lane_Exp_Calculator
         1880675, 1985675, 2100675, 2225675, 2360675, 2505675, 2665675, 2840675,
         3030675, 3235675, 3455675, 3696675, 3958675, 4241675, 4545675};
 
-        readonly int[] experience_table_ur = {0, 120, 360, 720, 1200, 1800, 2520, 3360, 4320,
+        private readonly int[] experience_table_ur = {0, 120, 360, 720, 1200, 1800, 2520, 3360, 4320,
         5400, 6600, 7920, 9360, 10920, 12600, 14400, 16320, 18360, 20520, 22800, 25200,
         27720, 30360, 33120, 36000, 39000, 42120, 45360, 48720, 52200, 55800, 59520,
         63360, 67320, 71400, 75600, 79920, 84360, 88920, 93600, 98400, 103440, 108720,
@@ -80,6 +80,7 @@ namespace Azur_Lane_Exp_Calculator
                 case 1:
                     table = experience_table_ur;
                     break;
+
                 default:
                     table = experience_table_enjp;
                     break;
